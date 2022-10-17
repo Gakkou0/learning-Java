4de14d3e993bd49c0a0017bc0572360b7a7b28fb
Gakkou0 <fhugo.625@gmail.com>
2022-10-17 08:38:42 -0300

    refatoração

--- Lista4/App/src/main/java/q/app/Funcionario.java
+++ Lista4/App/src/main/java/q/app/Funcionario.java
@@ -1,20 +1,33 @@
 package q.app;
 
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author fhugo
  */
-public class Funcionario {
+public final class Funcionario {
+
     private String cpf;
     private String nome;
     private char sexo;
     private double salarioBruto;
+    private GregorianCalendar dataNascimento;
+    private GregorianCalendar dataAdmicao;
 
-    public Funcionario(String cpf, String nome, char sexo, double salarioBruto) {
+    public Funcionario(String cpf, String nome, char sexo, double salarioBruto, int ano, int mes, int dia) {
         this.cpf = cpf;
         this.nome = nome;
         this.sexo = sexo;
         this.salarioBruto = salarioBruto;
+        setDataAdmicao(ano, mes, dia);
+    }
+    
+    public void setDataAdmicao(){
+    }
+    
+    public boolean validateCpf(String cpf){
+        return cpf.length() == 14;
     }
 
     public String getCpf() {
@@ -48,6 +61,13 @@ public class Funcionario {
     public void setSalarioBruto(double salarioBruto) {
         this.salarioBruto = salarioBruto;
     }
-    
-    
+
+    public void setDataAdmicao(int dia, int mes, int ano) {
+        dataNascimento = new GregorianCalendar(ano, mes, dia);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
